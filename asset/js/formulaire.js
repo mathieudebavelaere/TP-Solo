dec968c5f6f80cca33e87addb58dfa6cf7a7f9ae
mathieudebavelaere <mathieu.debavelaere@hotmail.com>
2022-12-15 16:25:03 +0100

    modif du 15dec

--- asset/js/formulaire.js
+++ asset/js/formulaire.js
@@ -1,4 +1,60 @@
-//-------------------------------------------Formulaire D'Inscription
+
+/*------------------blockFirstName---------------------*/
+const blockFirstName = document.getElementById('blockFirstName');
+
+blockFirstName.addEventListener('click', () => {
+    placeFirstName.classList.remove('textL');
+    placeFirstName.classList.add('textS');
+})
+/*------------------blockLastName---------------------*/
+
+/*------------------blockFirstName---------------------*/
+const blockLastName = document.getElementById('blockLastName');
+
+blockLastName.addEventListener('click', () => {
+    placeLastName.classList.remove('textL');
+    placeLastName.classList.add('textS');
+})
+/*------------------blockLastName---------------------*/
+
+/*------------------blockBirthDay---------------------*/
+const blockBirthDay = document.getElementById('blockBirthDay');
+
+blockBirthDay.addEventListener('click', () => {
+    placeBirthDay.classList.remove('textBL');
+    placeBirthDay.classList.add('textBS');
+})
+/*------------------blockLastName---------------------*/
+
+/*------------------blockBirthDay---------------------*/
+const blockMails = document.getElementById('blockMails');
+
+blockMails.addEventListener('click', () => {
+    placeMails.classList.remove('textL');
+    placeMails.classList.add('textS');
+})
+/*------------------blockLastName---------------------*/
+
+/*------------------blockMails---------------------*/
+const blockpassWord = document.getElementById('blockpassWord');
+
+blockPassWord.addEventListener('click', () => {
+    placePassWord.classList.remove('textPWL');
+    placePassWord.classList.add('textPWS');
+})
+/*------------------blockMails---------------------*/
+
+/*------------------blockMails---------------------*/
+const blockconfPassWord = document.getElementById('blockConfPassWord');
+
+blockConfPassWord.addEventListener('click', () => {
+    placeConfPassWord.classList.remove('textPWL');
+    placeConfPassWord.classList.add('textPWS');
+})
+/*------------------blockMails---------------------*/
+
+
+//-------------------------------------------Validation-REGEX
 
 
 const validateFormulaire = document.getElementById('validateFormulaire');
@@ -6,16 +62,16 @@ console.log(validateFormulaire);
 
 
 //-----------------------firstName
-validateFormulaire.addEventListener ('click', ()=> {
+validateFormulaire.addEventListener('click', () => {
     const firstName = document.getElementById('firstName').value;
     console.log(firstName);
 
     const controlF = /^[a-zA-Z]+$/;
 
     if (controlF.test(firstName)) {
-        alert ('ok');
+        blockFirstName.style.border = 'solid rgba(65, 65, 65, 0.426) 2px';
     } else {
-        alert ('mauvaise réponse mais fonctionne firstName');
+        blockFirstName.style.border = 'solid rgb(194, 46, 46) 2px';
     }
 })
 //-----------------------LastName
@@ -26,66 +82,66 @@ validateFormulaire.addEventListener('click', () => {
     const controlL = /^[a-zA-Z]+$/;
 
     if (controlL.test(lastName)) {
-        alert ('ok');
+        blockLastName.style.border = 'solid rgba(65, 65, 65, 0.426) 2px';
+
     }
     else {
-        alert ('mauvaise réponse mais fonctionne LastName');
-
+        blockLastName.style.border = 'solid rgb(194, 46, 46) 2px';
     }
 })
 
-//-----------------------Date
+//-----------------------BirthDay
 validateFormulaire.addEventListener('click', () => {
     const birthDay = document.getElementById('birthDay').value;
     console.log(birthDay);
 
     const controlDate = /^\d{4}[\-](0?[1-9]|1[012])[\-](0?[1-9]|[12][0-9]|3[01])$/;
 
-        if (controlDate.test(birthDay)) {
-            alert ('ok');
-        }
-        else {
-            alert ('mauvaise réponse mais fonctionne birthDay');
-    
-        }
+    if (controlDate.test(birthDay)) {
+        blockBirthDay.style.border = 'solid rgba(65, 65, 65, 0.426) 2px';
+    }
+    else {
+        blockBirthDay.style.border = 'solid rgb(194, 46, 46) 2px';
+    }
 })
 
 //-----------------------Email
 validateFormulaire.addEventListener('click', () => {
-    const eMail = document.getElementById('eMail').value;
+    const eMail = document.getElementById('mails').value;
     console.log(eMail);
 
     const controlM = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
 
-        if (controlM.test(eMail)) {
-            alert ('ok');
-        }
-        else {
-            alert ('mauvaise réponse mais fonctionne eMail');
-    
-        }
-    
+    if (controlM.test(eMail)) {
+        blockMails.style.border = 'solid rgba(65, 65, 65, 0.426) 2px';
+    }
+    else {
+        blockMails.style.border = 'solid rgb(194, 46, 46) 2px';
+    }
+
 })
 
 //-----------------------passWord
 
-validateFormulaire.addEventListener('click', ()=> {
+validateFormulaire.addEventListener('click', () => {
     const passWord = document.getElementById('passWord').value;
-console.log(passWord);
+    console.log(passWord);
 
-const confPassWord = document.getElementById('confPassWord').value;
-console.log(confPassWord);
+    const confPassWord = document.getElementById('confPassWord').value;
+    console.log(confPassWord);
 
-const controlP = /^([a-zA-Z]{8,8}[0-9]{1,1})$/;
+    const controlP = /^([a-zA-Z]{8,8}[0-9]{1,1})$/;
 
-if (controlP.test(passWord)){
-    if (passWord == confPassWord) {
-       alert('ok mots de passe identique');
-
+    if (controlP.test(passWord)) {
+        if (passWord == confPassWord) {
+            blockPassWord.style.border = 'solid rgba(65, 65, 65, 0.426) 2px';
+            blockConfPassWord.style.border = 'solid rgba(65, 65, 65, 0.426) 2px';
+        } else {
+            blockPassWord.style.border = 'solid rgb(194, 46, 46) 2px';
+            blockConfPassWord.style.border = 'solid rgb(194, 46, 46) 2px';
+        }
     } else {
-        alert ('mot de pas non identique');
+        blockPassWord.style.border = 'solid rgb(194, 46, 46) 2px';
+        blockConfPassWord.style.border = 'solid rgb(194, 46, 46) 2px';
     }
-} else {
-    alert ('minimum 8 caractères et 1 nombre')
-}
 })
